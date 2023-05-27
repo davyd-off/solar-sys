752651f1652095c1cf6bd7e18e60f0bfcde796e3
Zhc1561 <132933544+Zhc1561@users.noreply.github.com>
2023-05-27 17:39:58 +0700

    Документирование функций
    Функции в коде были про документированы.
    Программа работает исправно.
    Замечание:
         Добавить коэффициент изменения скорости на экран.
         Добавить кнопку для закрытия иконок с информацией о планетах.

--- SolarSystem.py
+++ SolarSystem.py
@@ -27,7 +27,7 @@ close_button_sprite.rect = close_button_rect
 all_sprites.add(close_button_sprite, layer=1)
 
 # Создание поверхности для канваса 
-info_planet_img = pygame.image.load("none_menu.png")
+info_planet_img = pygame.image.load(os.path.join("code-solar-sys","none_menu.png"))
 
 #-------------------------------------------------------------------
 # Загрузка изображении планет и информации
@@ -106,70 +106,146 @@ background_way_rect.centery = screen.get_rect().centery
 
 
 def print_text(message, x, y, font_color = (0,0,0), font_type = "Arial.ttf", font_size = 20):
-    font_type = pygame.font.SysFont(font_type, font_size)
-    text = font_type.render(message, True, font_color)
-    screen.blit(text, (x, y))
-    
+	"""
+	Функция отвечает за вывод текста на экран.
+
+	Args:
+		message - текст, который нужно вывести на экран
+		x - координата по оси X, где должен быть расположен левый край текста
+		y - координата по оси Y, где должен быть расположен верхний край текста
+		font_color - цвет текста. По умолчанию равен черному цвету (0, 0, 0)
+		font_type - тип шрифта. По умолчанию равен "Arial.ttf"
+		font_size - размер шрифта. По умолчанию равен 20 
+	"""
+	font_type = pygame.font.SysFont(font_type, font_size)
+	text = font_type.render(message, True, font_color)
+	screen.blit(text, (x, y))
+	
 
 
 class Planet:
-    def __init__(self, name, image, info, angle, distance, std_period, period, radius, x, y):
-        self.name = name
-        self.image = image
-        self.info = info
-        self.angle = angle
-        self.distance = distance
-        self.std_period = std_period
-        self.period = period
-        self.radius = radius
-        self.x = x
-        self.y = y
-        self.select = False
+	def __init__(self, name, image, info, angle, distance, std_period, period, radius, x, y):
+		"""
+		Конструктор класса Planet
+		
+		Args:
 
-    def update_position(self, center_x, center_y):
-        self.angle += 0.05 * (1 / self.period)
-        self.x = center_x + math.cos(self.angle) * self.distance
-        self.y = center_y + math.sin(self.angle) * self.distance
-        # if len(self.past_positions) < 128*abs(self.period):
-        #     self.past_positions.append((self.x, self.y))
-        
+		name - имя объекта
+		image - изображение объекта
+		info - дополнительная информация об объекте
+		angle - начальный угол объекта (в радианах)
+		distance - расстояние от центра координат до объекта
+		std_period - стандартный период обновления позиции объекта
+		period - текущий период обновления позиции объекта
+		radius - радиус объекта
+		x - координата x объекта
+		y - координата y объекта
+		"""
+		self.name = name
+		self.image = image
+		self.info = info
+		self.angle = angle
+		self.distance = distance
+		self.std_period = std_period
+		self.period = period
+		self.radius = radius
+		self.x = x
+		self.y = y
+		self.select = False
+
+	def update_position(self, center_x, center_y):
+		"""
+		Функция обновляет позицию объекта, который движется по окружности с центром в (center_x, center_y) 
+		и радиусом self.distance.
+		
+		Args:
+
+		center_x - координата x центра окружности
+		center_y - координата y центра окружности
+		"""
+		self.angle += 0.05 * (1 / self.period)
+		self.x = center_x + math.cos(self.angle) * self.distance
+		self.y = center_y + math.sin(self.angle) * self.distance
+		# if len(self.past_positions) < 128*abs(self.period):
+		#     self.past_positions.append((self.x, self.y))
+		
 
 
-    def draw(self, screen):
-        image_rect = self.image.get_rect()
-        image_rect.center = (int(self.x), int(self.y))
-        screen.blit(self.image, image_rect)
+	def draw(self, screen):
+		"""
+		Функция отвечает за отрисовку объекта на экране.
+		
+		Args:
 
-    # def draw_trail(self, screen):
-    #     for i in range(1, len(self.past_positions)):
-    #         pygame.draw.line(screen, (255, 153, 0), self.past_positions[i-1], self.past_positions[i], 2)
-    
-    def show_planet_info(self):
-        global info_planet_img
-        self.selected()
-        info_planet_img = self.info
+		screen - объект pygame.display, на котором будет отрисован объект
+		"""
+		image_rect = self.image.get_rect()
+		image_rect.center = (int(self.x), int(self.y))
+		screen.blit(self.image, image_rect)
 
-    def check_planet_clicked(self, mouse_pos):
-        dx = mouse_pos[0] - self.x
-        dy = mouse_pos[1] - self.y
-        dist = math.sqrt(dx**2 + dy**2)
-        
-        return dist <= self.radius
+	# def draw_trail(self, screen):
+	#     for i in range(1, len(self.past_positions)):
+	#         pygame.draw.line(screen, (255, 153, 0), self.past_positions[i-1], self.past_positions[i], 2)
+	
+	def show_planet_info(self):
+		"""
+		Функция отвечает за вывод информации о планете на экран.
+		"""
+		global info_planet_img
+		self.selected()
+		info_planet_img = self.info
 
-    def update_period(self):
-        if self.name != "Earth":
-            self.change_period(earth.period/(earth.std_period/self.std_period))
-        elif self.period >1.17 and self.period < 1.23:
-            self.change_period(self.std_period)
+	def check_planet_clicked(self, mouse_pos):
+		"""
+		Функция отвечает за проверку, была ли планета кликнута мышью.
+		
+		Args:
 
-    def change_period(self, set_period):
-        self.period = set_period
+		mouse_pos - кортеж координат мыши (x, y)
 
-    def selected(self):
-        self.select = True    
-    
-    def un_selected(self):
-        self.select = False
+		Returns:
+
+		Возвращает значение True, если расстояние между планетой и точкой, в которой была кликнута мышь, меньше или равно радиусу планеты. 
+		В противном случае возвращает значение False.
+		"""
+		dx = mouse_pos[0] - self.x
+		dy = mouse_pos[1] - self.y
+		dist = math.sqrt(dx**2 + dy**2)
+		
+		return dist <= self.radius
+
+	def update_period(self):
+		"""
+		Функция отвечает за обновление периода вращения планеты.
+		"""
+		if self.name != "Earth":
+			self.change_period(earth.period/(earth.std_period/self.std_period))
+		elif self.period >1.17 and self.period < 1.23:
+			self.change_period(self.std_period)
+
+	def change_period(self, set_period):
+		"""
+		Функция отвечает за изменение периода вращения планеты на заданное значение.
+		
+		Args:
+
+		set_period - новое значение периода вращения планеты
+		"""
+		self.period = set_period
+
+	def selected(self):
+		"""
+		Функция относится к классу, описывающему графический элемент, и отвечет за выделение элемента.
+
+		"""
+		self.select = True    
+	
+	def un_selected(self):
+		"""
+		Функция относится к классу, описывающему графический элемент, и отвечет за снятие выделение элемента.
+
+		"""
+		self.select = False
 
 
 sun = Planet("Sun", sun_image, sun_image_info, 0, 0, 0, 0, 100, screen.get_width() / 2, screen.get_height() / 2)
@@ -185,34 +261,61 @@ neptune = Planet("Neptune", neptune_image, neptune_image_info, 0, 370, 164.8, ea
 planets = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
 
 def plus_speed():
-    if earth.period>0.25:
-        earth.period -= earth.std_period*0.1 if earth.period < 1.21 else earth.std_period*0.4
-        # print(neptune.period,"\n")
-        print("Click ")
-        print(earth.period,"\n")
+	"""
+	Функция отвечает за ускорение вращения Земли.
+	"""
+	if earth.period>0.25:
+		earth.period -= earth.std_period*0.1 if earth.period < 1.21 else earth.std_period*0.4
+		# print(neptune.period,"\n")
+		print("Click ")
+		print(earth.period,"\n")
 def minus_speed():
-    if earth.period<5:
-        earth.period += earth.std_period*0.4  if earth.period > 1.19 else earth.std_period*0.1
-        # print(neptune.period,"\n")
-        print("Click ")
-        print(earth.period,"\n")
+	"""
+	Функция отвечает за замедление вращения Земли.
+	"""
+	if earth.period<5:
+		earth.period += earth.std_period*0.4  if earth.period > 1.19 else earth.std_period*0.1
+		# print(neptune.period,"\n")
+		print("Click ")
+		print(earth.period,"\n")
 
 class Button:
-    def __init__(self, width, height, action = None):
-        self.width = width
-        self.height = height
-        self.color = (13,162,58)
-        self.action = action
-        self.butt_rect = None
+	def __init__(self, width, height, action = None):
+		"""
+		Конструктор класса Button. 
 
-    def draw(self, x, y, message):
-        self.butt_rect = pygame.Rect(x, y, self.width, self.height)
-        pygame.draw.rect(screen, self.color, self.butt_rect)
-        print_text(message, x+20, y+12)
+		Args:
 
-    def do_action(self):
-        if self.action is not None:
-            self.action()
+		width - ширина кнопки
+		height - высота кнопки
+		action - функция, которая будет вызываться при нажатии на кнопку. По умолчанию равна None.
+		"""
+		self.width = width
+		self.height = height
+		self.color = (13,162,58)
+		self.action = action
+		self.butt_rect = None
+
+	def draw(self, x, y, message):
+		"""
+		Функция отвечает за отображение кнопки на экране.
+
+		Args:
+
+		x - координата по оси X левого верхнего угла кнопки на экране
+		y - координата по оси Y левого верхнего угла кнопки на экране
+		message - текст, который будет отображаться на кнопке
+		"""
+		self.butt_rect = pygame.Rect(x, y, self.width, self.height)
+		pygame.draw.rect(screen, self.color, self.butt_rect)
+		print_text(message, x+20, y+12)
+
+	def do_action(self):
+		"""
+		Функция отвечает за вызов функции, связанной с кнопкой, если она была передана в конструкторе.
+		"""
+		if self.action is not None:
+			self.action()
 
 button_plus = Button(200,50, plus_speed)
 button_minus = Button(200,50, minus_speed)
@@ -234,64 +337,64 @@ clicked_time = 0
 delay = 0
 
 while running:
-    # Обработка событий в pygame
-    for event in pygame.event.get():
-        delay = 300
-        clicked = False
-        for planet in planets:
-            # Проверка на выход
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if not clicked and button_plus.butt_rect.collidepoint(event.pos) and pygame.time.get_ticks() - clicked_time > delay:
-                    clicked = True
-                    button_plus.do_action()
-                    clicked_time = pygame.time.get_ticks()
-                if not clicked and button_minus.butt_rect.collidepoint(event.pos) and pygame.time.get_ticks() - clicked_time > delay:
-                    clicked = True
-                    button_minus.do_action()
-                    clicked_time = pygame.time.get_ticks()
-                # Обработка клика на кнопке закрытия
-                if close_button_rect.collidepoint(event.pos):
-                    running = False
-                # Проверка клика по планете    
-                if planet.check_planet_clicked(event.pos):
-                    planet.show_planet_info()
-            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                clicked = False            
-    
+	# Обработка событий в pygame
+	for event in pygame.event.get():
+		delay = 300
+		clicked = False
+		for planet in planets:
+			# Проверка на выход
+			if event.type == pygame.QUIT:
+				running = False
+			elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+				if not clicked and button_plus.butt_rect.collidepoint(event.pos) and pygame.time.get_ticks() - clicked_time > delay:
+					clicked = True
+					button_plus.do_action()
+					clicked_time = pygame.time.get_ticks()
+				if not clicked and button_minus.butt_rect.collidepoint(event.pos) and pygame.time.get_ticks() - clicked_time > delay:
+					clicked = True
+					button_minus.do_action()
+					clicked_time = pygame.time.get_ticks()
+				# Обработка клика на кнопке закрытия
+				if close_button_rect.collidepoint(event.pos):
+					running = False
+				# Проверка клика по планете    
+				if planet.check_planet_clicked(event.pos):
+					planet.show_planet_info()
+			elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+				clicked = False            
+	
 
 
 
-    # Фоновое изображение
-    screen.blit(background_image, (0, 0))
-    screen.blit(background_way, background_way_rect)
+	# Фоновое изображение
+	screen.blit(background_image, (0, 0))
+	screen.blit(background_way, background_way_rect)
 
-    # button_minus.draw(30,640,"Замедлить")
-    # button_plus.draw(250,640,"Ускорить")
+	# button_minus.draw(30,640,"Замедлить")
+	# button_plus.draw(250,640,"Ускорить")
 
-    button_minus.draw(minus_pos,760,"Замедлить")
-    button_plus.draw(plus_pos,760,"Ускорить")
+	button_minus.draw(minus_pos,760,"Замедлить")
+	button_plus.draw(plus_pos,760,"Ускорить")
 
-    # Отображение солнца
-    sun.draw(screen)
-    # print(len(jupiter.past_positions),"\n")
-    # Рисование и обновление положение планет
-    for planet in planets[1:]:
-        planet.update_position(sun.x, sun.y)
-        planet.update_period()
-        # planet.draw_trail(screen)
-        planet.draw(screen)
-    change_procent = 0
-    all_sprites.draw(screen)
-    screen.blit(info_planet_img, (40, 40))
-    # Обновление дисплея
-    pygame.display.update()
+	# Отображение солнца
+	sun.draw(screen)
+	# print(len(jupiter.past_positions),"\n")
+	# Рисование и обновление положение планет
+	for planet in planets[1:]:
+		planet.update_position(sun.x, sun.y)
+		planet.update_period()
+		# planet.draw_trail(screen)
+		planet.draw(screen)
+	change_procent = 0
+	all_sprites.draw(screen)
+	screen.blit(info_planet_img, (40, 40))
+	# Обновление дисплея
+	pygame.display.update()
 
-    
+	
 
-    # Пауза (кадры в секунду)
-    clock.tick(fps)
+	# Пауза (кадры в секунду)
+	clock.tick(fps)
 
 # Выход из pygame
 pygame.quit()
